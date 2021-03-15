d03a0441a245df4db062d952021cc89773f72066
Alexander Schwartzberg <aeksco@gmail.com>
2021-03-14 23:53:51 -0400

    Removed unused PageHeader component from next app

--- develop/next/pages/_app.tsx
+++ develop/next/pages/_app.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { PageHeader } from "../src/PageHeader";
 import App from "next/app";
 import "../styles/index.css";
 
@@ -9,7 +8,6 @@ export default class extends App {
 
         return (
             <div className={"h-full pb-32 bg-gray-100"}>
-                <PageHeader></PageHeader>
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-full dark:bg-gray-800 bg-gray-100 dark:text-gray-200">
                     <div className="grid grid-cols-1">
                         <div className="col-span-1 mt-5">
