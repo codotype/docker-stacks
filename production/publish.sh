0ee78ceb5d2c7237e38d35cf911dce9141278f46
Alexander Schwartzberg <aeksco@gmail.com>
2021-03-28 18:03:59 -0400

    Updated publish.sh

--- production/publish.sh
+++ production/publish.sh
@@ -1,3 +1,3 @@
 # docker build -t codotype-plugin-prod .
-# docker tag bb38976d03cf codotype/codotype-plugin-prod:latest
+# docker tag ac57b99c4eb5 codotype/codotype-plugin-prod:latest
 # docker push codotype/codotype-plugin-prod
